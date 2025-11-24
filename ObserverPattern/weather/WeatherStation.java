2c79b05c242f8c6730a912cc46ef09ce1fd4244a
XsoratoX <tensho19990117@gmail.com>
2025-11-24 16:41:43 +0900

    Add HeatIndex.java which compute Heate Index and display it. And add a line to WeatherStation.java related to HeatIndex.

--- ObserverPattern/weather/WeatherStation.java
+++ ObserverPattern/weather/WeatherStation.java
@@ -6,6 +6,7 @@ public class WeatherStation {
         CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
         StaticsDisplay staticsDisplay = new StaticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
