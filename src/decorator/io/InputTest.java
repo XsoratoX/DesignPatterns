117606c5ea9faa17fcc274b29d6a7e8fe12739c8
XsoratoX <tensho19990117@gmail.com>
2025-12-07 20:47:48 +0900

    Add two examples for decorator pattern.

---
+++ src/decorator/io/InputTest.java
@@ -0,0 +1,27 @@
+package decorator.io;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+        int c;
+
+        try {
+            InputStream in = new LowerCaseInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(
+                                    "/Users/iimoritensho//Desktop/programs/github/DesignPatternsForObjectOrientedSoftware/DesignPatterns/src/decorator/io/test.txt")));
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
